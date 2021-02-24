ab636207d2de6fa6b7fa73005b6da0ed2c1a915d
Amir Vazquez <j.amir.vazquez@gmai.com>
2021-02-23 23:59:35 -0800

    Create Create DB.sql

---
+++ Resources/DDL/Create DB.sql
@@ -0,0 +1,12 @@
+-- Database: Brazilian E-Commerce
+
+-- DROP DATABASE "Brazilian E-Commerce";
+
+CREATE DATABASE "Brazilian E-Commerce"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
