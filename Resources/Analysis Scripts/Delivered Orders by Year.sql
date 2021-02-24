827ae02badd56f0bbd5fa66d4b018b7d38b056d2
Amir Vazquez <j.amir.vazquez@gmai.com>
2021-02-24 00:08:37 -0800

    Update Delivered Orders by Year.sql

--- Resources/Analysis Scripts/Delivered Orders by Year.sql
+++ Resources/Analysis Scripts/Delivered Orders by Year.sql
@@ -1,8 +1,11 @@
-select dt.year_actual,dt.quarter_name, sum(cast(price as bigint)) as "Price_sum" 
+select 
+delivered_dt.year_actual,
+delivered_dt.quarter_name, sum(cast(price as bigint)) as "Price_sum" 
 
 from public.orders o 
 join public.order_items oi on o.order_id = oi.order_id
-join date_dim dt on dt.date_dim_id = o.sk_order_delivered_customer_dt	
+join date_dim delivered_dt on delivered_dt.date_dim_id = o.sk_order_delivered_customer_dt	
+
 where o.order_status ='delivered'
-group by dt.year_actual,dt.quarter_name
+group by delivered_dt.year_actual,delivered_dt.quarter_name
 
