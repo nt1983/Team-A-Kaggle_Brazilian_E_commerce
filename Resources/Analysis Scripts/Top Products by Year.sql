7bd85edab7c70aa086c40be1612e838ca23f3f70
Amir Vazquez <j.amir.vazquez@gmai.com>
2021-02-24 19:13:44 -0800

    update documents

--- Resources/Analysis Scripts/Top Products by Year.sql
+++ Resources/Analysis Scripts/Top Products by Year.sql
@@ -1,6 +1,8 @@
 select 
 delivered_dt.year_actual,
 delivered_dt.quarter_name, 
+p.product_category_name,
+
 sum(cast(price as bigint)) as "Price_sum",
 count(o.order_id) as "Order_cnt"
 
@@ -8,6 +10,12 @@ from public.orders o
 join public.order_items oi on o.order_id = oi.order_id
 join date_dim delivered_dt on delivered_dt.date_dim_id = o.sk_order_delivered_customer_dt	
 
+join public.products p on p.product_id = oi.product_id
+
 where o.order_status ='delivered'
-group by delivered_dt.year_actual,delivered_dt.quarter_name
+group by delivered_dt.year_actual,
+delivered_dt.quarter_name,
+p.product_category_name
+
+order by sum(cast(price as bigint)) desc
 
